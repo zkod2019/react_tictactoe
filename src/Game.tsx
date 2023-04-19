3278436f934dc0432234c05d161feda4492d3427
Zaya Kod <zkod777@gmail.com>
2023-04-19 15:47:50 +0400

    fix: now player can press in any square

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,20 +1,70 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Square from './Square'
+
 const INITIAL_GAME_STATE = ["","","","","","","","",""]
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function Game() {
   const [gameState, setGameState] =  useState(INITIAL_GAME_STATE)
-  const [currentPLayer, setCurrentPlayer] = useState("X")
+  const [currentPlayer, setCurrentPlayer] = useState("X")
+
+  useEffect(() => {
+    checkForWinner()
+  }, [gameState])
   
+  const checkForWinner = () => {
+    let roundWon = false
+    for (let i = 0; i < WINNING_COMBOS.length; i++){
+      const winCombo = WINNING_COMBOS[i];
+      let a = gameState[winCombo[0]]
+      let b = gameState[winCombo[1]]
+      let c = gameState[winCombo[2]]
+
+      if ([a, b, c].includes("")){
+        continue
+      }
+
+      if (a === b && b === c){
+        roundWon = true 
+        break;
+      }
+    }
+    if (roundWon){
+      window.alert(`Congrats player ${currentPlayer}! You're the winner`)
+      return
+    }
+
+    if (!gameState.includes("")) {
+      window.alert("Game is a draw!")
+      // setTimeout(() => handleDraw(), 500);
+      return;
+    }
+
+    changePlayer();
+
+  }
+  const changePlayer = () => {
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  }
   const handleCellClick = (event: any) => {
-    const cellIndex = Number(event.target.getAttribute("data-call-index"))
+    const cellIndex = Number(event.target.getAttribute("data-cell-index"))
     const currentValue = gameState[cellIndex]
    if (currentValue){
     return
    }
 
-   const newValues = [...gameState]
-   newValues[cellIndex] = currentPLayer
+   const newValues = [...gameState];
+   newValues[cellIndex] = currentPlayer;
+   setGameState(newValues);
   };
   return <div className="h-full p-8 test-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
     <h1 className="text-center text-5xl mb-4 font-display text-white">
